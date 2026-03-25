69797561a5af144074e3cce655c3d8ced7be20f5
Matheus Freitas <mf022rj@gmail.com>
2026-03-25 13:59:53 -0300

    Initial setup with Java and Gradle

---
+++ src/main/java/com/example/mfstudio/MfstudioApplication.java
@@ -0,0 +1,13 @@
+package com.example.mfstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mfstudio/MfstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mfstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
